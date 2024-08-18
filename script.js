f557dea4a503315912bc92a86faea5a2d1eea679
bySawR <marcusaevensen@gmail.com>
2024-08-19 00:07:19 +0200

    s

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('show');
-  }
+}
+  
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const hero = document.querySelector(".hero");
+    const lines = hero.innerHTML.split(/<br\s*\/?>/g); // Split by lines (considering line breaks)
+    
+    hero.innerHTML = lines.map((line, index) => `<div class="line" style="animation-delay:${index * 0.5}s">${line}</div>`).join("");
+  });
+
